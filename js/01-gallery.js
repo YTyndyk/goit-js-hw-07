774145d5db22c9a1902723fbc1234ed0a0aea9da
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-24 12:31:43 +0200

    создает разметку в HTML

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gelleryRef = document.querySelector(".gallery");
+const cardMarkup = createGalleryItems(galleryItems);
+
+gelleryRef.insertAdjacentHTML("beforeend", cardMarkup);
+
+function createGalleryItems(galleryItems) {
+	return galleryItems
+		.map(({ preview, original }) => {
+			return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image description"
+    />
+  </a>
+</div>
+`;
+		})
+		.join("");
+}

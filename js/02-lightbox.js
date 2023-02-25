f290195231339d5346cdb924e569e55d2b4e7768
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-25 19:12:24 +0200

    рендерит галлерею и подключает  библиотеку

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gelleryRef = document.querySelector(".gallery");
+const cardMarkup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+gelleryRef.addEventListener("click", onImgClick);
+
+gelleryRef.insertAdjacentHTML("beforeend", cardMarkup);
+
+function onImgClick(e) {
+	e.preventDefault();
+	if (e.target.nodeName !== "IMG") {
+		return;
+	}
+
+	let gallery = new SimpleLightbox(".gallery a");
+	gallery.on("show.simplelightbox", function () {
+		// do something…
+	});
+}
+
+function createGalleryItems(galleryItems) {
+	return galleryItems
+		.map(({ preview, original }) => {
+			return `<a class="gallery__item"
+   href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+           alt="Image description"
+    />
+</a>
+`;
+		})
+		.join("");
+}

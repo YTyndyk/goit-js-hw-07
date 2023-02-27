7b586a27868fe6c181dff38b7837a7ef643ee7b7
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-27 09:16:32 +0200

    dona task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,20 +5,21 @@ const cardMarkup = createGalleryItems(galleryItems);
 
 gelleryRef.insertAdjacentHTML("beforeend", cardMarkup);
 
-new SimpleLightbox(".gallery a");
-gallery.on("show.simplelightbox", function () {
-	// do something…
+new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
 });
 
 function createGalleryItems(galleryItems) {
 	return galleryItems
-		.map(({ preview, original }) => {
+		.map(({ preview, original, description }) => {
 			return `<a class="gallery__item"
    href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-           alt="Image description"
+           alt="${description}"
     />
 </a>
 `;
